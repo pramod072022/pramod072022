144ec8f158c66c75b0a924140372a2b287d4153e
pramod <pramodtoom@gmail.com>
2025-06-03 11:29:12 +0530

    spring

---
+++ Downloads/spring-dev-demo/src/main/java/com/edureka/spring_dev_demo/SpringDevDemoApplication.java
@@ -0,0 +1,13 @@
+package com.edureka.spring_dev_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDevDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDevDemoApplication.class, args);
+	}
+
+}
